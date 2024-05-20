3726740a2af5c0b61bf7310402b3a3ee0e7cdfe8
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2024-05-20 19:43:37 +0100

    Logging in users

--- auth/domain/src/main/java/com/example/auth/domain/AuthRepository.kt
+++ auth/domain/src/main/java/com/example/auth/domain/AuthRepository.kt
@@ -5,5 +5,6 @@ import com.example.core.domain.util.EmptyResult
 
 
 interface AuthRepository {
+    suspend fun login(email: String, password: String): EmptyResult<DataError.Network>
     suspend fun register(email: String, password: String): EmptyResult<DataError.Network>
 }
