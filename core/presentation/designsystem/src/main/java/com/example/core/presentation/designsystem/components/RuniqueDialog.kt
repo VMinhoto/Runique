bde2e7fceb0d923bef48bbab0cc6d942a4038d38
Vitor <up201303086@fe.up.pt>
2024-05-31 17:40:52 +0100

    Polyline bugfix and pause run logic

--- core/presentation/designsystem/src/main/java/com/example/core/presentation/designsystem/components/RuniqueDialog.kt
+++ core/presentation/designsystem/src/main/java/com/example/core/presentation/designsystem/components/RuniqueDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,9 +27,9 @@ fun RuniqueDialog(
     title: String,
     onDismiss: () -> Unit,
     description: String,
-    primaryButton: @Composable () -> Unit,
+    primaryButton: @Composable RowScope.() -> Unit,
     modifier: Modifier = Modifier,
-    secondaryButton: @Composable () -> Unit = {},
+    secondaryButton: @Composable RowScope.() -> Unit = {},
 ){
     Dialog(onDismissRequest = onDismiss){
         Column(
